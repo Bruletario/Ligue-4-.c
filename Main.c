0fb91cfd116068595ee3fa6252eb07ced53aa3c2
Bruletario <brunoalmeidaacademico@gmail.com>
2026-01-22 22:11:26 -0300

    Finalizacao da estrutura visual
    terminei a logica da estrutura visual e deixei um teste.

--- Main.c
+++ Main.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
-
+  
 #define LINHAS 6
 #define COLUNAS 7
 //valores p matriz
@@ -14,7 +13,7 @@ const int player2 = 2;
 
 struct jogador{
     char nome[50]; //nome do player
-    int vitórias; //vítorias que ele tem
+    int vitorias; //vítorias que ele tem
     int jogadas; //quantas jogadas ele fez na partida atual
 };
 
@@ -26,36 +25,82 @@ struct partida{
 
 //== PARTE VISUAL ==//
 
-const char *cor_p1 = "\x1b[34m"; //cor azul para o p1
-const char *cor_p2 = "\x1b[31m"; //cor vermelha para o p2
-const char *ficha = "█";
-const char *tabuleiro_vazio = " ";
+const char cor_reset[] = "\033[0m"; //padrao
+const char cor_p1[] = "\033[1;34m"; // Azul
+const char cor_p2[] = "\033[1;31m"; // Vermelho
+const char ficha[] = "█";          // ficha do jogo
+const char tabuleiro_vazio[] = " ";
 
-int limpar_tela(){
+void limpar_tela(){
     #ifdef _WIN32 //verica se é o win e limpa a tela
     system("cls");
     #elif __linux__ //verica se é o linux e limpa a tela
     system("clear");
-    #endif;
+    #endif
 }
 
-int exibir_cabecalho(){
-    printf("== LIGUE 4++ ==\n");
-    printf("1   2   3   4   5   6   7\n");
+void configurar_ambiente_win(){ //garantir que nao vai ter problerma do bloco no windows
+    #ifdef _WIN32
+    system("chcp 65001");
+    #endif
+}
+
+void exibir_cabecalho(){
+    printf("         LIGUE 4++  \n");
+    printf("  1   2   3   4   5   6   7  \n");
 }
 
 
 
-int desenhar_tabuleiro(struct partida jogo){
+void desenhar_tabuleiro(struct partida jogo){
 
     limpar_tela();
     exibir_cabecalho();
-    printf("--------------------------");
 
-    for(int i=0;i<5;i++){
-        printf("|");
-            for(int i=0; j<6; j++){
-                partida.matriz[i][j];
+    printf("+---+---+---+---+---+---+---+\n");
+
+    for(int i=0; i<LINHAS; i++){
+        printf("|"); 
+        
+        for(int j=0; j<COLUNAS; j++){
+            int valor = jogo.matriz[i][j];
+            
+            if(valor == vazio){
+                printf(" %s |", tabuleiro_vazio); //verifica se a coordenada é 0, se for deixa em branco
+            } 
+            else if(valor == player1){
+                
+                printf(" %s%s%s |", cor_p1, ficha, cor_reset); //verifica se a coordenada é 1, se for troca para o quadrado e pinta de azul
+            } 
+            else if(valor == player2) {
+                printf(" %s%s%s |", cor_p2, ficha, cor_reset); //verifica se a coordenada é 2, se for troca para o quadrado e pinta de vermelho
             }
-
+        }
+        printf("\n"); 
+        
+        printf("+---+---+---+---+---+---+---+\n"); 
     }
+}
+
+
+/*teste de visuzalizacao
+int main() {
+
+    struct partida jogoAtual;
+    
+    for(int i=0; i<LINHAS; i++) {
+        for(int j=0; j<COLUNAS; j++) {
+            jogoAtual.matriz[i][j] = 0;
+        }
+    }
+    
+    // simulando uma jogada
+    jogoAtual.matriz[5][3] = 1;
+    jogoAtual.matriz[4][3] = 2; 
+    
+    desenhar_tabuleiro(jogoAtual);
+
+    getchar(); //pausa pra ver o resultado
+
+    return 0;
+} */
